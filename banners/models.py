4613103c94487bfa3a351d037f4520a700fc8f06
Richelly Italo <richellyitalo@gmail.com>
2019-03-27 00:37:02 -0300

    Exclusao de arquivo no delete

--- banners/models.py
+++ banners/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from imagekit.models import ProcessedImageField, ImageSpecField
 from imagekit.processors import ResizeToFill
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+import os
 
 
 class Banner(models.Model):
@@ -24,3 +27,16 @@ class Banner(models.Model):
 
     def __str__(self):
         return self.descricao
+
+    # def delete(self, using=None, keep_parents=False):
+    #     print('Excluindo imagem')
+    #     print('imagem', self.imagem.path)
+    #     os.remove(self.file.path)
+    #     os.remove(self.imagem.path)
+    #     return super().delete(using, keep_parents)
+
+
+@receiver(pre_delete, sender=Banner)
+def handler_pre_delete(sender, instance, **kwargs):
+    os.remove(instance.imagem.path)
+    print('sender', sender)
